c7ac98a26fbfdf2320a2757b9283632f10fc70f2
Shailesh Ladumor <shaileshmladumor@gmail.com>
2021-07-14 08:49:27 +0530

    fix: remove extra code

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,31 +6,10 @@
     <title>Laravel Package Tutorial</title>
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-    <!-- Styles -->
-    <style>
 
-    </style>
 </head>
 <body>
 <div id="root"></div>
-{{--
-<div class="imgg"></div>
-<div class="flex-center position-ref full-height">
-    <div class="content">
-        <div class="title m-b-md">
-            How To Install
-            <a style="color: #61dafb"  href="https://github.com/shailesh-ladumor/one-signal">React JS</a>  in
-            <span style="color: #ff2d20">
-                <strong>Laravel</strong>
-            </span>
-        </div>
-        <br>
-        <div class="links">
-            <a href="#"><strong style="font-size: 60px">- By Shailesh Ladumor</strong></a>
-        </div>
-    </div>
-</div>
- --}}
 </body>
 <script type="text/javascript" src="{{ mix('js/app.js') }}"></script>
 </html>
