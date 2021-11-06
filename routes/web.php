00ee38f2d3694afac67f6a4b05ab4cec6606acf5
Shailesh Ladumor <shaileshmladumor@gmail.com>
2021-11-06 19:08:17 +0530

    feat package development

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/swagger-docs', function () {
+    return view('swagger.index');
 });
