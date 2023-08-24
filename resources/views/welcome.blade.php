1f4a70d12604c30d003c917093570382e615cd7d
Shailesh Ladumor <shaileshmladumor@gmail.com>
2023-08-24 17:53:47 +0530

    fix: make default layout

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -15,22 +15,15 @@
 <div class="flex-center position-ref full-height">
     <div class="content">
         <div class="title m-b-md">
-{{--             <a style="color: #61dafb" href="https://github.com/MohmmedAshraf/blade-flags">Blade Flags</a> in--}}
+             <a target="_blank" style="color: #61dafb" href="#">Package name</a> in
             <span style="color: #ff2d20">
-                <strong>Laravel Blade Directives - Part 2</strong>
+                <strong>Laravel</strong>
             </span>
         </div>
         <br>
         <div class="links">
             <a href="{{route('demo')}}"><strong style="font-size: 60px">- By Shailesh Ladumor</strong></a>
         </div>
-        {{-- <marquee direction="down" width="950" height="300" behavior="alternate" style="border:none">
-            <marquee behavior="alternate">
-                <div class="links" style="margin-top: 50px">
-                    <a href="#"><strong style="font-size: 80px; color:red; font-weight: bold">Subscribe Now</strong></a>
-                </div>
-            </marquee>
-        </marquee> --}}
         <img style="margin-left:1565px" id="hideMe" src="https://cliply.co/wp-content/uploads/2020/01/372001730_SUBSCRIBE_BUTTON_400px.gif">
     </div>
 </div>
