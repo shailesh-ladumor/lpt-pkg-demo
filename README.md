2fbb02089f16676ac67440b8c019759c73eddc8d
Shailesh Ladumor <shaileshmladumor@gmail.com>
2023-08-31 17:07:07 +0530

    feat: add laravel react basic setup

--- README.md
+++ README.md
@@ -1,7 +1,37 @@
-## NOTE
-All demo available into the branch. please check branch dropdown for tutorials source code.
+### Step 1. Create Laravel Project
+### Step 2. Install Required npm packages
+```angular2html
+        "@popperjs/core": "^2.11.8",
+        "@vitejs/plugin-react": "^4.0.4",
+        "bootstrap": "^5.3.1",
+        "react": "^18.2.0",
+        "react-dom": "^18.2.0",
+        "sass": "^1.66.1"
+```
+or add in dependencies
+
+```angular2html
+"dependencies": {
+  "react-refresh": "^0.14.0"
+ }
+```
+and fire `npm install`
+## Step 3 Install the laravel/ui package
+
+```angular2html
+ composer require laravel/ui
+```
+
+## Step 4 Configure React in Laravel
+
+```angular2html
+php artisan ui react
+```
+## Configure `vite.config.js`
 
 
-## Video Tutorials
+```angular2html
+npm run dev
 
-[<img src="https://img.youtube.com/vi/yMtsgBsqDQs/0.jpg" width="580">](https://www.youtube.com/c/LaravelPackageTutorial)
+php artisan serve
+```
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,25 +8,10 @@
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
     <!-- Styles -->
     <link href="{{asset('app.css')}}" rel="stylesheet">
+    @vitereactrefresh
+    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
 </head>
 <body>
-
-<div class="imgg"></div>
-<div class="flex-center position-ref full-height">
-    <div class="content">
-        <div class="title m-b-md">
-            <a target="_blank" style="color: #61dafb" href="#">Package name</a> in
-            <span style="color: #ff2d20">
-                <strong>Laravel</strong>
-            </span>
-        </div>
-        <br>
-        <div class="links">
-            <strong style="font-size: 60px">- By Shailesh Ladumor</strong>
-        </div>
-        <img style="margin-left:1565px" id="hideMe"
-             src="https://cliply.co/wp-content/uploads/2020/01/372001730_SUBSCRIBE_BUTTON_400px.gif">
-    </div>
-</div>
+<div id="example"></div>
 </body>
 </html>
